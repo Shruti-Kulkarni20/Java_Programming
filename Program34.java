76ab2e5c1cd2a7cb68b236ea9bd31b6cbb29f006
Shruti-Kulkarni20 <shrutikulkarni503@gmail.com>
2026-02-03 22:27:01 +0530

    java Program

---
+++ Program34.java
@@ -0,0 +1,63 @@
+// Input 4  4   
+/*
+
+    o   *   *   *
+    o   o   *   *
+    o   o   o   *
+    o   o   o   o
+    
+*/
+
+import java.util.*;
+
+class Pattern
+{
+    public void Display(int iRow,int iCol)
+    {
+        int i = 0,j = 0;
+
+        if(iRow == iCol)
+        {
+            System.out.println("Invalid input");
+        }
+
+        for(i = 1;i <= iRow;i++)
+        {
+            for(j = 1;j <= iCol;j++)
+            {
+                if(j <= i)
+                {
+                    System.out.print("o\t");
+                }
+                else
+                {
+                    System.out.print("*\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Program34
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1 = 0, iValue2 = 0;
+
+        System.out.println("Enter number of rows : ");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter number of columns : ");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+
+        pobj.Display(iValue1, iValue2);
+
+        sobj.close();
+        
+    }
+    
+}
